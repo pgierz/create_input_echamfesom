df10b5d288f1e300d215a0067128ecd56c0c45e1
Paul Gierz <pgierz@awi.de>
2020-08-27 09:02:22 +0200

    cleanup cli

--- src/awiesm_bc/cli.py
+++ src/awiesm_bc/cli.py
@@ -1,18 +1,12 @@
 import click
-import crayons
-from loguru import logger
 
 import sys
-import subprocess
 
 
 @click.group()
 def main(args=None):
     """Console script for awiesm_bc"""
     return 0
-
-
-
 
 if __name__ == "__main__":
     sys.exist(main())
