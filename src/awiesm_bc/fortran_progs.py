4bcd5b36120c2f847a6ec146abb3bd799dc9c3a6
Paul Gierz <pgierz@awi.de>
2020-08-27 12:09:07 +0200

    compiles jsbach init prog

--- src/awiesm_bc/fortran_progs.py
+++ src/awiesm_bc/fortran_progs.py
@@ -1,9 +1,9 @@
-import crayons
 from loguru import logger
 
+import os
 import pkgutil
 import subprocess
-import tempfile
+import xdg.BaseDirectory
 
 from awiesm_bc.cli import main
 from awiesm_bc.config import get_config
@@ -14,33 +14,49 @@ def _get_fortran_program(prog_name):
     Gets a string representation of the ``FORTRAN`` program specified by
     ``prog_name``, assuming it is under the ``src/fortran`` subdir.
     """
-    return pkgutil.get_data("awiesm_bc", f"../fortran/{prog_name}")
+    return pkgutil.get_data("awiesm_bc", f"../fortran/{prog_name}").decode()
 
 
 @main.command()
 def compile_jsbach_init_file():
     """Compiles the jsbach init file program"""
-    config = get_config()
-    jsbach_init_file_str = _get_fortran_program("jsbach_init_file.f90")
-    jsbach_init_file = tempfile.NamedTemporaryFile()
-    jsbach_init_file.write(jsbach_init_file_str)
-    print(jsbach_init_file.name)
-    import pdb; pdb.set_trace()
-    command_to_run = f"{config('fc', namespace='jsbach_init_file')} {jsbach_init_file.name}"
-    # command_to_run = "lalala exit 3"
     logger.info("--> Compiling pre-requisite program: jsbach_init_file.f90")
+    config = get_config()
+    # This gets a string representation of the jsbach_init_file.f90 program,
+    # packaged together with the rest of the stuff here, similar to how
+    # open(<filename>).read() would do.
+    jsbach_init_file_str = _get_fortran_program("jsbach_init_file.f90")
+    # Now make a folder to put it:
+    os.makedirs(f"{xdg.BaseDirectory.get_runtime_dir()}/awiesm_bc", exist_ok=True)
+    # And actually write the program to disk...
+    # FIXME(PG): Probably, there is a less dumb way to do this...
+    jsbach_init_file = open(
+        f"{xdg.BaseDirectory.get_runtime_dir()}/awiesm_bc/jsbach_init_file.f90",
+        mode="w",
+    )
+    jsbach_init_file.write(jsbach_init_file_str)
+    # Figure out if the user has super special choices for the fortran
+    # compiler, flibs, and fflags
+    fc = config("fc", namespace="jsbach_init_file")
+    flibs = config("flibs", namespace="jsbach_init_file")
+    fflags = config("fflags", namespace="jsbach_init_file")
+    # Tell the world what you will try to compile:
+    command_to_run = f"{fc} {fflags} {flibs} {jsbach_init_file.name} -o {jsbach_init_file.name.replace('.f90', '')}"
     logger.debug(command_to_run)
     try:
+        # Compile the dude:
         subprocess.check_output(command_to_run, shell=True)
     except subprocess.CalledProcessError as e:
+        # Something fucked up. Tell us what went wrong:
         error_message = e.output.decode()
         logger.error("ERROR! Sorry...")
         if error_message:
             logger.error(f"{error_message}")
         return e.returncode
     else:
+        # Nothing fucked up. Give back the path to the program you just compiled...
         logger.success("...done!")
-        return 0
+        return f"{jsbach_init_file.name.replace('.f90', '')}"
 
 
 # PG For testing:
